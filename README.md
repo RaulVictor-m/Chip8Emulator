7966950739edcca1d6ed2e0bfe1314d4b56571c3
RaulVictor-m <raulvictor822@gmail.com>
2022-12-23 04:03:01 -0300

    CMake

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 A Simple implementation of the Chip8 emulator
 
 Still a work in Progress, There are some things needing implementation, such as sound, the debugLog system and a few bug fixes.
-
-
-For now you it uses a MVSC .sln to compile, but every thing you need to compile it is there.
--- src/Main.cpp
+++ src/Main.cpp
@@ -4,8 +4,15 @@
 int main()
 {
 	Chip8 emulator;
-	
-	if (emulator.InitEmulator("IMPORTED-ROMS/TETRIS") && emulator.Construct(64 * 1, 32 * 1, 12, 12))
+	while (true) {
+
+		std::cout << "PUT THE PATH OF THE CHOSEN ROM:\n";
+		char buffer[200];
+		std::cin >> buffer;
+		if (emulator.InitEmulator(buffer))break;
+		std::cout << "NOT VALID TRY AGAIN\n";
+	}
+	if (emulator.Construct(64 * 1, 32 * 1, 12, 12))
 		emulator.Start();
 
 	return 0;
